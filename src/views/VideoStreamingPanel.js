baf6b511fda278fcf72f786376e52d6c5d80a2b4
tjdus <dusml0277@gmail.com>
2024-12-06 20:15:56 +0900

    page view

--- src/views/VideoStreamingPanel.js
+++ src/views/VideoStreamingPanel.js
@@ -2,6 +2,8 @@ import {Header, Panel} from '@enact/sandstone/Panels';
 import {PanelContext} from './Context';
 import {useCallback, useContext} from 'react';
 import Video from './Video';
+import { PageViews, Page } from '@enact/sandstone/PageViews';
+import Item from '@enact/sandstone/Item';
 
 const VideoStreamingPanel = props => {
 	const {data, ...rest} = props;
@@ -10,10 +12,25 @@ const VideoStreamingPanel = props => {
     
 	return (
 		<Panel {...rest}>
-            <Header title={`Video`} />
+			<Header title={`Video`} />
+		<PageViews
+			pageIndicatorType="dot"
+			>
+			<Page>
 			<Video data={data} />
-		</Panel>
-	);
+			</Page>
+			<Page aria-label="This is a description for page 1">
+	
+				<Item>
+					Item 1
+				</Item>
+				<Item>
+					Item 2
+				</Item>
+			</Page>
+			</PageViews>
+			</Panel>
+	)
 };
 
 export default VideoStreamingPanel;
