cd2c534b87a6b408dc6abc62ac24955b3959146e
lsoyeon <yfresian@LAPTOP-T5MIHS0D>
2024-12-06 19:15:28 +0900

    작업 중인 변경 사항 커밋

--- src/views/VideoStreamingPanel.js
+++ src/views/VideoStreamingPanel.js
@@ -8,46 +8,48 @@ const VideoStreamingPanel = props => {
 	const {data, ...rest} = props;
     const index = data?.index ?? 0;
 	const {setPanelData} = useContext(PanelContext);
-    // //const video = data?.video;  // Video data 가져오기 추가
-	// if (!video) return null;
-	// /*부가정보 검색용*/
-    // const handleIngredientClick = (ingredient) => {
-    //     setPanelData(prev => [...prev, {name: 'search', data: {query: ingredient}}]);
-    // };
+    const video = data?.video;  // Video data 가져오기 추가
+	if (!video) return null;
+	/*부가정보 검색용*/
+    const handleIngredientClick = (ingredient) => {
+        setPanelData(prev => [...prev, {name: 'search', data: {query: ingredient}}]);
+    };
 
-    // const handleFoodNameClick = () => {
-    //     setPanelData(prev => [...prev, {name: 'search', data: {query: data.video.foodName}}]);
-    // };
+    const handleFoodNameClick = () => {
+        setPanelData(prev => [...prev, {name: 'search', data: {query: data.video.foodName}}]);
+    };
 
-	return (
-		<Panel {...rest}>
-            <Header title={`Video`} />
-			<Video data={data} />
-			
+return (
+    <Panel scrollable {...rest}>
+        <Header title={`Video`} />
+        
+        <Row wrap style={{marginTop: '2rem'}}>
+            <Cell size="100%">
+                <Video data={data} />
+            </Cell>
+            <Cell size="50%">
+                <h3 onClick={handleFoodNameClick} style={{cursor: 'pointer', color: 'blue'}}>
+                    요리 이름: {data.video.foodName}
+                </h3>
+            </Cell>
+            <Cell size="50%">
+                <h4>재료:</h4>
+                <ul>
+                    {data.video.ingredients.map((ingredient, index) => (
+                        <li
+                            key={index}
+                            onClick={() => handleIngredientClick(ingredient)}
+                            style={{cursor: 'pointer', color: 'blue'}}
+                        >
+                            {ingredient}
+                        </li>
+                    ))}
+                </ul>
+            </Cell>
+        </Row>
+    </Panel>
+);
 
-            {/* <Row wrap style={{marginTop: '2rem'}}>
-                <Cell size="50%">
-                    <h3 onClick={handleFoodNameClick} style={{cursor: 'pointer', color: 'blue'}}>
-                        요리 이름: {data.video.foodName}
-                    </h3>
-                </Cell>
-                <Cell size="50%">
-                    <h4>재료:</h4>
-                    <ul>
-                        {data.video.ingredients.map((ingredient, index) => (
-                            <li
-                                key={index}
-                                onClick={() => handleIngredientClick(ingredient)}
-                                style={{cursor: 'pointer', color: 'blue'}}
-                            >
-                                {ingredient}
-                            </li>
-                        ))}
-                    </ul>
-                </Cell>
-            </Row> */}
-		</Panel>
-	);
 };
 
 export default VideoStreamingPanel;
